16c8f108cad37091200cf8a98bb0b464fabc4ab4
Ashton Graves <agraves10@unl.edu>
2023-06-22 09:32:44 -0500

    Adds gfal copy and sum work

--- gracc-backup.py
+++ gracc-backup.py
@@ -1,6 +1,7 @@
 import sys
 import tomllib
 import os
+import subprocess
 
 id = sys.argv[1]
 
@@ -13,6 +14,18 @@ secondary_path = f'/var/lib/graccarchive/{id}/secondary'
 
 for file in os.listdir(os.fsencode(input_path)):
     file_name = os.fsdecode(file)
-    if file_name.endswith("tar.gz"):
-        file_size = os.stat(input_path + '/' + file_name).st_size
-        print(file_size)
+    file_full_path = input_path + '/' + file_name
+    file_size = os.stat(file_full_path).st_size
+    if file_size == 0 or not file_name.endswith('tar.gz'):
+        pass
+    elif file_size < 300:
+        os.remove(file)
+    else:
+        os.environ['X509_USER_CERT'] = '/etc/grid-security/backup-cert/gracc.opensciencegrid.org-cert.pem'
+        os.environ['X509_USER_KEY'] = '/etc/grid-security/backup-cert/gracc.opensciencegrid.org-key.pem'
+        local_full_path = "file://" + file_full_path
+        remote_full_path = output_path + file_name
+        print(subprocess.check_output(f'gfal-copy {local_full_path} {remote_full_path}', shell=True))
+        remote_checksum = subprocess.check_output(f'gfal-sum {remote_full_path} MD5', shell=True)
+        local_checksum = subprocess.check_output(f'gfal-sum {file_full_path} MD5', shell=True)
+        print(remote_checksum == local_checksum)
